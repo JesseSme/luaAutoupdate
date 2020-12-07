36d069ad4652d20e756a96c552e366a5cdec9243
Jesse <jesse.smedberg@outlook.com>
2020-12-07 20:41:13 +0200

    Update updater.lua
    Authentication added

--- updater.lua
+++ updater.lua
@@ -5,11 +5,58 @@
 local git = require("git")
 local programs = git.programLinks
 
-local auth = {["Jesse"]="VaikeaSalasana1234"}
+local auth = {["users"]={"Jesse"},
+              ["passwords"]={"vaikeasalasana1234"}}
+local loggedIn = true
 
-local function serve(user, pass)
-    if not user ==  then
+
+local function checkUser(user)
+    for key, value in ipairs(auth["users"]) do
+        if value == user then
+            return key
+        end
+    end
+    return nil
+end
+
+
+local function checkPassword(pass, key)
+    if auth["passwords"][key] == pass then
+        return 1
+    end
+    return 0
+end
+
+local function authenticate()
+    user = io.read()
+    pass = io.read()
+    key = checkUser(user)
+    if key == nil then
+        print("User not found.")
+        return 0
+    end
+    if not checkPassword(pass, key) then
+        print("Wrong password.")
+        return 1
     end
 end
+
+--Add proximity log out to this.
+local function serve()
+    while loggedIn do
+        if authenticate() then
+            print("Logged in!")
+            term.setCursorPos(1, 1)
+            print("Serving programs...")
+            loggedIn = false
+        else
+            do return end
+        end
+    end
+    while true do
+        
+    end
+end
+
 
 serve(arg[1], arg[2])
