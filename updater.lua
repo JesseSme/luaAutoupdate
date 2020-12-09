108fdca31952c63939d012f519f149468060180d
Jesse <jesse.smedberg@outlook.com>
2020-12-09 16:29:21 +0200

    Update updater.lua

--- updater.lua
+++ updater.lua
@@ -112,7 +112,7 @@ local function serve(user, pass)
             return 0
         end
     end
-    os.startTimer(1)
+    os.startTimer(360)
     modem = peripheral.wrap(modem_side)
     modem.open(in_signal)
     monitor = peripheral.wrap("right")
