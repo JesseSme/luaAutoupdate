c2d9d5f84bd027dedd767c784879ef4f61e53b8a
Jesse <jesse.smedberg@outlook.com>
2020-12-10 15:49:29 +0200

    Update cipherer.lua

--- autoupdate/cipherer.lua
+++ autoupdate/cipherer.lua
@@ -4,13 +4,12 @@ local cipherer = {}
 function cipherer.cipher(mode, text)
     if mode == "encrypt" then
         return peripheral.call("cipher_0", mode, text)
-    else if mode == "decrypt" then
+    elseif mode == "decrypt" then
         return peripheral.call("cipher_0", mode, text)
     else
         print("No valid mode for cipher set.")
     end
     return nil
-    end
 end
 
 
