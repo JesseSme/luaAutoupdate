e126f2e988e70e0faccd15c8b6072736873f9a77
JesseSme <jesse.smedberg@outlook.com>
2020-12-11 15:19:09 +0200

    Create serial.lua

---
+++ tools/serial.lua
@@ -0,0 +1,23 @@
+local serial = {}
+
+
+function serializeMessage(msg)
+    return textutils.serialize(msg)
+end
+
+
+function deserializeMessage(msg)
+    local deserialized_msg = nil
+    if not (type(msg) == "string") then
+        print("Bad request.")
+        return 0
+    end
+    deserialized_msg = textutils.unserialize(msg)
+    for key, value in pairs(deserialized_msg) do
+        print(tostring(key)..": "..tostring(value))
+    end
+    return deserialized_msg
+end
+
+
+return serial
